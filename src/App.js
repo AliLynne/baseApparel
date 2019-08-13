1b0db89d6784e655db7460ca0f66d5d3f826ce9f
AliLynne <alilynne.thompson@gmail.com>
2019-08-13 16:44:06 -0700

    lots of stuff

--- src/App.js
+++ src/App.js
@@ -5,28 +5,28 @@ import hero from './images/hero-mobile.jpg'
 function App() {
   return (
     <Fragment>
-      <header>
-        <img src={logo} alt="logo" />
-        <img src={hero} alt="woman" />
+      <header className="header">
+        <img src={logo} alt="logo" className="header_logo"/>
+        <img src={hero} alt="woman" className="header_hero"/>
       </header>
 
-      <main>
-        <h1><span>We're</span> coming soon</h1>
-        <p>Hello fellow shoppers! We're currently building our new fashion store.
+      <main className="main">
+        <h1 className="main_headline"><span className="main_headline-pop">We're</span> coming soon</h1>
+        <p className="main_text">Hello fellow shoppers! We're currently building our new fashion store.
   Add your email below to stay up-to-date with announcements and our launch deals.</p>
 
-        <form>
-          <label>
+        <form className="form">
+          <label className="form_label">
             Email Address
-            <input type="text" name="email" />
-            <input type="submit" value=">" />
+            <input className="form_input" type="text" name="email" />
+            <input className="form_submit" type="submit" value=">" />
           </label>
         </form>
       </main>
       
   
   
-  <footer>
+  <footer className="footer">
     <p className="attribution">
       Challenge by <a href="https://www.frontendmentor.io?ref=challenge" target="_blank" rel="noopener noreferrer">Frontend Mentor</a>. 
       Coded by <a href="#">AliLynne</a>.
